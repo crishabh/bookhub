7efa08579cc8990ab4defdab12ce699485b804b2
supriyak <rishabh.332@gmail.com>
2014-12-11 18:04:29 +0530

    bookhub

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,20 @@
+class HomeController < ApplicationController
+  def home
+
+  end
+  def aboutus
+
+  end
+  def contactus
+
+  end
+  def assignment
+
+  end
+  def sign_up
+
+  end
+  def show
+
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookhub::Application.config.secret_token = '2b89b22d135abaae8b04ab8b626b2cdcad7e444bcd37cec37bbc698fef510acf50fb6efff5d1f84e6f271797d7edb3b0c41d6f2c7bd4e2d146a9b2543bec3abe'
